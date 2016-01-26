50ad74f614c1e26d14acbab78f1754099c4f8809
drakemain <demain1990@gmail.com>
2016-01-26 15:13:08 -0800

    cache user data locally

--- src/form-handler.js
+++ src/form-handler.js
@@ -1,7 +1,9 @@
 "use strict";
 
-var Promise = require('bluebird');
-var request = require('request');
+var Promise  = require('bluebird');
+var request  = require('request');
+var path     = require('path');
+var fs       = require('fs');
 
 var validate = require('./validate');
 var SteamSigError = require('./error');
@@ -17,8 +19,14 @@ exports.renderProfile = function(key, uInput) {
   })
 
   .then(function(steamid) {
-    var URI = buildURI(key, steamid);
+    var URI = buildURI(key, "ISteamUser/GetPlayerSummaries/v0002", steamid);
     return getUserData(URI);
+  })
+
+  .then(function(userData) {
+    userData.lastAPICall = new Date();
+    writeUserData(userData);
+    return Promise.resolve(userData);
   })
 
   .tap(function() {
@@ -32,6 +40,8 @@ exports.renderProfile = function(key, uInput) {
     console.timeEnd("imgProcess");
   })
 }
+
+exports.writeUserData = writeUserData;
 
 var getUserData = function(uri) {
 
@@ -53,7 +63,25 @@ var getUserData = function(uri) {
   });
 }
 
-var buildURI = function(APIkey, SteamID) {
-  return "https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key="
-    + APIkey + "&steamids=" + SteamID;
+var buildURI = function(APIkey, method, SteamID) {
+  return "https://api.steampowered.com/"
+    + method
+    + "/?key=" + APIkey
+    + "&steamids=" + SteamID;
+}
+
+var writeUserData = function(userData) {
+  return new Promise(function(resolve, reject) {
+    var filePath = path.join('assets', 'profiles', userData.steamid, 'userData.JSON');
+
+    var userDataString = JSON.stringify(userData);
+
+    fs.writeFile(filePath, userDataString, function(err) {
+      if (err) {
+        reject(err);
+      } else {
+        resolve()
+      }
+    })
+  })
 }
