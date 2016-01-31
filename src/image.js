efbb139ae7fb045c41210756fb792517b52f124f
drakemain <demain1990@gmail.com>
2016-01-30 16:43:17 -0800

    removed unnecessary nested promise

--- src/image.js
+++ src/image.js
@@ -1,7 +1,6 @@
 "use strict";
 
-var fs    = require('fs'),
-    path  = require('path'),
+var path  = require('path'),
     gm    = require('gm'),
     Promise = require('bluebird'),
     parseSteam = require("./parser");
@@ -9,63 +8,45 @@ var fs    = require('fs'),
 module.exports = function (userInfo) {
   return new Promise(function(resolve, reject) {
 
-    getUserDirectory(userInfo.steamid)
-    .then(function(userDir) {
-      var filePath = path.join(userDir, 'sig.png')
+    var userDir = userInfo.userDirectory;
+    
+    var filePath = path.join(userDir, 'sig.png')
 
-      var img = gm()
-      .in('-page', '+0+0')
-      .in('assets/img/base-gray.png')
+    var img = gm()
+    .in('-page', '+0+0')
+    .in('assets/img/base-gray.png')
 
-      .in('-page', '+8+8')
-      .in(userInfo.avatarfull.replace("https", "http"))
+    .in('-page', '+8+8')
+    .in(userInfo.avatarfull.replace("https", "http"))
 
-      .font("Arial")
-      .fontSize(28)
-      .drawText(200, 28, userInfo.personaname);
+    .font("Arial")
+    .fontSize(28)
+    .drawText(200, 28, userInfo.personaname);
 
-      if (userInfo.communityvisibilitystate !== 3) {
-        img
-        .in('-page', '+0+0')
-        .in('assets/img/confidential.png');
-
-      } else {
-        var timeInfo = parseSteam.timecreated(userInfo.timecreated);
-
-        img
-        .fontSize(16)
-        .drawText(200, 45, parseSteam.personastate(userInfo.personastate))
-
-        //.drawText(200, 62, timeInfo.dateCreated)
-        .drawText(200, 77, timeInfo.age);
-
-      }
-      
+    if (userInfo.communityvisibilitystate !== 3) {
       img
-      .flatten()
+      .in('-page', '+0+0')
+      .in('assets/img/confidential.png');
 
-      .write(filePath, function(err) {
-        if (!err) {resolve(filePath);}
-        else {reject(err);}
-      });
+    } else {
+      var timeInfo = parseSteam.timecreated(userInfo.timecreated);
+
+      img
+      .fontSize(16)
+      .drawText(200, 45, parseSteam.personastate(userInfo.personastate))
+
+      //.drawText(200, 62, timeInfo.dateCreated)
+      .drawText(200, 77, timeInfo.age);
+
+    }
+    
+    img
+    .flatten()
+
+    .write(filePath, function(err) {
+      if (!err) {resolve(filePath);}
+      else {reject(err);}
     });
-
   });
+
 };
-
-function getUserDirectory(steamid) {
-  var userDir = path.join('assets', 'profiles', steamid);
-
-  return new Promise(function (resolve) {
-    fs.stat(userDir, function(err, stats) {
-
-      if (!stats) {
-        fs.mkdir(userDir, function() {
-          resolve(userDir);
-        });
-
-      } else {resolve(userDir);}   
-    });
-  });
-
-}
