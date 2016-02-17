3b5dd4a4be2253ea55d1973e016faf8e70c29307
drakemain <demain1990@gmail.com>
2016-02-16 17:27:57 -0800

    now using gm's composite method

--- src/image.js
+++ src/image.js
@@ -3,57 +3,67 @@
 var path  = require('path'),
     gm    = require('gm'),
     Promise = require('bluebird'),
-    parseSteam = require("./parser");
+    parseSteam = require('./parser')
 
 module.exports = function (userInfo) {
   console.time("imgProcess");
-  var filePath = path.join(userInfo.userDirectory, 'sig.png')
+  var filePath = path.join(userInfo.userDirectory, 'sig.png');
 
-  return new Promise(function(resolve, reject) {      
-    var img = gm()
-    .in('-page', '+0+0')
-    .in(path.join('assets', 'img', 'base-gray.png'))
+  return composite(path.join('assets', 'img', 'base-gray.png'), 
+    userInfo.avatarfull.replace('https', 'http'))
 
-    .in('-page', '+8+8')
-    .in(userInfo.avatarfull.replace("https", "http"))
+  .then(function(buffer) {
+    return new Promise(function(resolve, reject) {
+      var img = gm(buffer)
+      .drawLine(200, 8, 491, 8)
+      .drawLine(200, 8, 200, 191)
+      .drawLine(491, 191, 200, 191)
+      .drawLine(491, 191, 491, 8)
 
-    .flatten()
+      .font("Arial").fontSize(20).drawText(208, 32, userInfo.personaname);
 
-    .drawLine(200, 8, 491, 8)
-    .drawLine(200, 8, 200, 191)
-    .drawLine(491, 191, 200, 191)
-    .drawLine(491, 191, 491, 8)
+      if (userInfo.communityvisibilitystate !== 3) {
+        img
+        .in('-page', '+0+0')
+        .in('assets/img/confidential.png');
 
-    .font("Arial").fontSize(28).drawText(208, 34, userInfo.personaname);
-
-    if (userInfo.communityvisibilitystate !== 3) {
-      img
-      .in('-page', '+0+0')
-      .in('assets/img/confidential.png');
-
-    } else {
-      img.fontSize(16)
-
-      if (userInfo.gameid) {
-        var gameStatus = "In-Game";
-        if (userInfo.currentGame) {gameStatus += ": " + userInfo.currentGame;}
-        img.drawText(216, 53, gameStatus);
       } else {
-        img.drawText(216, 53, parseSteam.personastate(userInfo.personastate));
+        img.fontSize(14)
+
+        if (userInfo.gameid) {
+          var gameStatus = "In-Game";
+          if (userInfo.currentGame) {gameStatus += ": " + userInfo.currentGame;}
+          img.drawText(216, 53, gameStatus);
+        } else {
+          img.drawText(216, 53, parseSteam.personastate(userInfo.personastate));
+        }
+
+        /*img.drawLine(447, 17, 482, 17)
+        .drawLine(447, 17, 447, 52)
+        .drawLine(482, 52, 447, 52)
+        .drawLine(482, 52, 482, 17)*/
+
+        img.drawText(410, 32, parseSteam.timecreated(userInfo.timecreated).age);
       }
-
-      /*img.drawLine(447, 17, 482, 17)
-      .drawLine(447, 17, 447, 52)
-      .drawLine(482, 52, 447, 52)
-      .drawLine(482, 52, 482, 17)*/
-
-      img.drawText(410, 29, parseSteam.timecreated(userInfo.timecreated).age);
-    }
-
-    img.write(filePath, function(err) {
-      console.timeEnd("imgProcess");
-      if (!err) {resolve(filePath);}
-      else {reject(err);}
+      img
+      .write(filePath, function(err) {
+        console.timeEnd("imgProcess");
+        if (!err) {resolve(filePath);}
+        else {reject(err);}
+      });
     });
   });
 };
+
+function composite(bottomLayer, topLayer) {
+  return new Promise(function(resolve, reject) {
+    gm().command("composite")
+    .in("-geometry", "+8+8")
+    .in(topLayer)
+    .in(bottomLayer)
+    .toBuffer('PNG', function(err, buffer) {
+      if (!err) {resolve(buffer);}
+      else {reject(err);}
+    })
+  });
+}
