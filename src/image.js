4461984bbb4e11f332fbf7ebb4ed714137abf0db
drakemain <demain1990@gmail.com>
2016-03-17 03:18:45 -0700

    can composite more than two images

--- src/image.js
+++ src/image.js
@@ -3,18 +3,21 @@
 var path  = require('path'),
     gm    = require('gm'),
     Promise = require('bluebird'),
-    parseSteam = require('./parser')
+    parseSteam = require('./parser');
+
+var img;
 
 module.exports = function (userInfo) {
-  console.time("|__imgProcess");
+  console.time("|>imgProcess");
   var filePath = path.join(userInfo.userDirectory, 'sig.png');
+  var tempFile = path.join(userInfo.userDirectory, 'temp.png');
 
-  return composite(path.join('assets', 'img', 'base-gray.png'), 
+  return compositeToBuffer(path.join('assets', 'img', 'base-gray.png'), 
     userInfo.avatarfull.replace('https', 'http'))
 
   .then(function(buffer) {
     return new Promise(function(resolve, reject) {
-      var img = gm(buffer)
+      img = gm(buffer)
       .drawLine(200, 8, 491, 8)
       .drawLine(200, 8, 200, 191)
       .drawLine(491, 191, 200, 191)
@@ -23,9 +26,10 @@ module.exports = function (userInfo) {
       .font("Arial").fontSize(20).drawText(208, 32, userInfo.personaname);
 
       if (userInfo.communityvisibilitystate !== 3) {
-        img
-        .in('-page', '+0+0')
-        .in('assets/img/confidential.png');
+        compositeToFile(filePath, path.join('assets', 'img', 'confidential.png'))
+        .tap(console.timeEnd("|>imgProcess"))
+
+        .then(resolve);
 
       } else {
         img.fontSize(14)
@@ -43,19 +47,18 @@ module.exports = function (userInfo) {
         .drawLine(482, 52, 447, 52)
         .drawLine(482, 52, 482, 17)*/
 
-        img.drawText(410, 32, parseSteam.timecreated(userInfo.timecreated).age);
+        img.drawText(410, 32, parseSteam.timecreated(userInfo.timecreated).age)
+        .write(filePath, function(err) {
+          console.timeEnd("|>imgProcess");
+          if (!err) {resolve(filePath);}
+          else {reject(err);}
+        });
       }
-      img
-      .write(filePath, function(err) {
-        console.timeEnd("|__imgProcess");
-        if (!err) {resolve(filePath);}
-        else {reject(err);}
-      });
     });
   });
 };
 
-function composite(bottomLayer, topLayer) {
+function compositeToBuffer(bottomLayer, topLayer) {
   return new Promise(function(resolve, reject) {
     gm().command("composite")
     .in("-geometry", "+8+8")
@@ -65,5 +68,20 @@ function composite(bottomLayer, topLayer) {
       if (!err) {resolve(buffer);}
       else {reject(err);}
     })
+  });
+}
+
+function compositeToFile(path, topLayer) {
+  return new Promise(function(resolve, reject) {
+    img.write(path, function(err) {
+      gm()
+      .command('composite')
+      .in(topLayer)
+      .in(path)
+      .write(path, function(err) {
+        if (!err) {resolve(path);}
+        else {reject(err);}
+      });
+    });
   });
 }
