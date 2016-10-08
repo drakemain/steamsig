4f2693ee45110df34e5b5ceb2aca5a65e14388b7
drakemain <demain1990@gmail.com>
2016-10-08 06:26:12 -0700

    additional steamid tests, vanity name tests moved
    Tests for steamid were rewritten to accomodate simplified purpose of the function. Additional tests were added for steamid.
    
    Vanity name resolution tests were moved to test/steam.js.

---
+++ test/steam.js
@@ -0,0 +1,25 @@
+require('dotenv').config({path: './config/.env'});
+
+var Promise = require('bluebird');
+var chai = require('chai');
+var chaiAsPromised = require('chai-as-promised');
+var assert = chai.assert;
+
+var steam = require('../src/steam');
+
+chai.use(chaiAsPromised);
+
+describe("Resolve Vanity Name", function() {
+  it("should return a steamid from valid vanity names", function() {
+    return Promise.all([
+      steam.resolveVanityName("l0mbax").then(function(result) {
+        assert.equal("76561197964880220", result);}),
+      steam.resolveVanityName("blaenk").then(function(result) {
+        assert.equal("76561197974257161", result);})
+    ]);
+  });
+
+  it("should return a validation error from invalid vanity names", function() {
+    return assert.isRejected(steam.resolveVanityName("thisisnotavalidsteamid"));
+  });
+});
--- test/validate.js
+++ test/validate.js
@@ -1,6 +1,6 @@
 require('dotenv').config({path: './config/.env'});
 
-var Promise = require('bluebird');
+// var Promise = require('bluebird');
 var chai = require('chai');
 var chaiAsPromised = require('chai-as-promised');
 var assert = chai.assert;
@@ -9,15 +9,25 @@ var validate = require('../src/validate');
 
 chai.use(chaiAsPromised);
 
-describe("steamid", function() {
-  it("should return input steamid if it is valid", function() {
-    return validate.steamid("76561197960419964").then(function(id) {
-      assert.equal("76561197960419964", id);
-    });
+describe("Steam ID Format", function() {
+  it("should return true if valid Steam ID format", function() {
+    assert(validate.steamid("76561197960419964"));
   });
 
-  it("should return a validation error for invalid steamids", function() {
-    return assert.isRejected(validate.steamid("76561191234567890"));
+  it("should return true if format is valid but is not an actual Steam ID", function() {
+    assert(validate.steamid("76561191234567890"));
+  });
+
+  it("should return false if does not match ID prefix", function() {
+    assert(!validate.steamid("12345671234567890"));
+  });
+
+  it("should return false if length is less than 17", function() {
+    assert(!validate.steamid("7656119123456789"));
+  });
+
+  it("should return false if length is greater than 17", function () {
+    assert(!validate.steamid("765611912345678900"));
   });
 });
 
@@ -41,20 +51,5 @@ describe("trim user input", function() {
     var trimmedInput = validate.trimUserInput(userInput);
 
     return assert.equal(trimmedInput, "HelloWorld");
-  });
-});
-
-describe("Resolve Vanity Name", function() {
-  it("should return a steamid from valid vanity names", function() {
-    return Promise.all([
-      validate.resolveVanityName("l0mbax").then(function(result) {
-        assert.equal("76561197964880220", result);}),
-      validate.resolveVanityName("blaenk").then(function(result) {
-        assert.equal("76561197974257161", result);})
-    ]);
-  });
-
-  it("should return a validation error from invalid vanity names", function() {
-    return assert.isRejected(validate.resolveVanityName("thisisnotavalidsteamid"));
   });
 });
