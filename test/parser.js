cc6966dc2eb4181530dbd54eb441c443ca55d38a
drakemain <demain1990@gmail.com>
2016-10-30 03:40:12 -0700

    add test for personastate parser

---
+++ test/parser.js
@@ -0,0 +1,34 @@
+var Promise = require('bluebird');
+var chai = require('chai');
+var chaiAsPromised = require('chai-as-promised');
+var assert = chai.assert;
+
+var parse = require('../src/parser');
+
+chai.use(chaiAsPromised);
+
+describe("personastate", function() {
+  it("should return 'Offline' when passed 0", function() {
+    assert.equal(parse.personastate(0), "Offline");
+  });
+
+  it("should return 'Online' when passed 1", function() {
+    assert.equal(parse.personastate(1), "Online");
+  });
+
+  it("should return 'Busy' when passed 2", function() {
+    assert.equal(parse.personastate(2), "Busy");
+  });
+
+  it("should return 'Away' when passed 3", function() {
+    assert.equal(parse.personastate(3), "Away");
+  });
+
+  it("should return 'Snooze' when passed 4", function() {
+    assert.equal(parse.personastate(4), "Snooze");
+  });
+
+  it("should return 'Looking to trade' when passed 5", function() {
+    assert.equal(parse.personastate(5), "Looking to trade");
+  });
+});
