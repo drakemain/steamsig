f9a99689c130dfab9e6307f130ff7e0c96918d2c
demain1990 <demain1990@gmail.com>
2016-11-03 22:01:47 -0700

    canvas renderer

--- src/render.js
+++ src/render.js
@@ -5,17 +5,23 @@ var request = require('request-promise');
 var Canvas = require('canvas');
 
 var Image = Canvas.Image;
-var canvas = new Canvas(500, 200);
-var ctx = canvas.getContext('2d');
+var canvas = new Canvas();
+var sig = canvas.getContext('2d');
 
 module.exports = function(userInfo) {
   var r = 100, g = 100, b = 100;
 
+  canvas.height = 200; canvas.width = 500;
+
   fillCanvas('rgb(' +r +',' +g +',' +b +')'); //background
-  ctx.strokeRect(200.5, 8.5, 291, 183);
+  sig.strokeRect(200, 8, 291, 183);
   drawText(userInfo.personaname, "Helvetica", "20px", 208, 32);
 
-  return placeImageByURL(userInfo.avatarfull, 8, 8).then(function() {
+  return placeImageByURL(userInfo.avatarfull, 8, 8, 1)
+
+  .then(placeImageByURL(userInfo.recentGameLogos[0], 209, 133, .5))
+
+  .then(function() {
     return new Promise(function(resolve) {
       var out = fs.createWriteStream(userInfo.sigPath);
       var stream = canvas.pngStream();
@@ -34,16 +40,19 @@ module.exports = function(userInfo) {
 
 
 function fillCanvas(color) {
-  ctx.save();
-  ctx.fillStyle = color;
-  ctx.fillRect(0, 0, canvas.width, canvas.height);
-  ctx.restore();
+  sig.save();
+  sig.fillStyle = color;
+  sig.fillRect(0, 0, canvas.width, canvas.height);
+  sig.restore();
 }
 
-function placeImageByURL(imgURL, x, y) {
+function placeImageByURL(imgURL, x, y, scale) {
+  if (!imgURL) {return;}
+
   return request.get({url: imgURL, encoding: null}, function(err, res, body) {
     if (err) {console.error(err);}
 
+    scale = scale || 1;
     var img = new Image();
 
     img.onerror = function(error) {
@@ -51,7 +60,13 @@ function placeImageByURL(imgURL, x, y) {
     };
 
     img.onload = function() {
-      ctx.drawImage(img, x, y);
+      sig.save()
+      var height = img.height * scale;
+      var width = img.width * scale;
+
+      sig.drawImage(img, x, y, width, height);
+
+      sig.restore();
     };
 
     img.src = new Buffer(body, 'binary');
@@ -61,10 +76,10 @@ function placeImageByURL(imgURL, x, y) {
 function drawText(text, font, size, x, y) {
   var thisFont = size + " " + font;
   console.log(thisFont);
-  ctx.save();
-  ctx.font = thisFont;
-  ctx.fillText(text, x, y);
-  ctx.restore();
+  sig.save();
+  sig.font = thisFont;
+  sig.fillText(text, x, y);
+  sig.restore();
 }
 
 // return new Promise(function(resolve, reject) {
