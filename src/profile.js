c52bcb291aea97756804d2e906ab058673125757
drakemain <demain1990@gmail.com>
2017-03-07 17:03:17 -0800

    profile manager overhaul

--- src/profile.js
+++ src/profile.js
@@ -10,130 +10,105 @@ var steam           = require('./steam');
 var SteamSigError   = require('./error');
 var _               = require('lodash');
 
+var steamid = '';
+
 exports.cache = getCache;
 
-exports.refresh = function(steamid) {
-  return getCache(steamid)
+exports.refresh = function(_steamid) {
+  steamid = _steamid;
 
+  return getCache()
+  
   .then(function(localCache) {
     if (isExpired(localCache.lastUpdated)) {
-      return updateProfile(steamid);
-    } else {
-      console.log('skip refresh');
+      return buildSteamCache()
+
+      .then(function(refreshedSteamCache) {
+        console.log('Found changes. Rendering profile.');
+
+        if (_.isEqual(refreshedSteamCache.steam, localCache.steam)) {
+          return updateSteamProfile(refreshedSteamCache);
+        } else {
+          console.log('No changes. Skipping profile render.');
+        }
+      });
     }
   })
 
   .catch(SteamSigError.FileDNE, function(err) {
-    var userDir = this.getDir(steamid);
-    var steamCache = path.join(userDir, 'steam.JSON');
+    var canvasPath = path.join('assets', 'profiles', steamid, 'canvas.JSON');
+    var steamPath = path.join('assets', 'profiles', steamid, 'steam.JSON');
+    
+    if (err.filePath === canvasPath) {
+      console.log('Caught canvas data DNE');
 
-    console.log('Caught File DNE', err.filePath);
-
-    if (err.filePath === steamCache) {
-      console.log('Creating new local steam cache');
-      return newSteamCache(steamid);
-    } else {
+      // canvas data must be set before sig can be drawn.
       throw err;
+    } else if (err.filePath === steamPath) {
+      console.log('Caught steam data DNE');
+
+      return buildSteamCache()
+
+      .then(updateSteamProfile);
     }
   });
 };
 
 exports.setCanvas = function(canvasData) {
   console.log('[SET CANVAS]');
-  // temporary until web form can generate all canvas data
-  var dataTemplate = getCanvasData();
-  var steamid = canvasData.steamid;
-  delete canvasData.steamid;
-  if (canvasData.recentGameLogos) {
-    // Will be able to build an array
-    // Won't be limited to 2
-    canvasData.recentGameLogo1 = {'active' : true};
-    canvasData.recentGameLogo2 = {'active' : true};
-  }
-  delete canvasData.recentGameLogos;
 
-  for (var element in canvasData) {
-    dataTemplate.elements[element].active = true;
-  }
+  steamid = canvasData.steamid;
 
-  return getCacheFile(steamid, 'canvas.JSON')
+  var compiledCanvasData = compileCanvasData(canvasData);
 
-  .then(function(cache) {
-    cache = dataTemplate;
+  return saveCache('canvas.JSON', compiledCanvasData)
 
-    return saveCache(steamid, 'canvas.JSON', cache)
-
-    .then(function() {
-      console.log('[/SET CANVAS]');
-    });
-  })
-
-  .catch(SteamSigError.FileDNE, function() {
-    console.log('Creating new Canvas.');
-    var newCache = {};
-    newCache = dataTemplate;
-    return saveCache(steamid, 'canvas.JSON', newCache)  
-
-    .then(function() {
-      console.log('[/SET CANVAS]');
-    });
+  .then(function() {
+    console.log('[/SET CANVAS]');
   });
 };
 
-exports.getDir = getDir = function(steamid) {
-  return path.join('assets', 'profiles', steamid);
-};
+exports.getDir = function(_steamid) {
+  return path.join('assets', 'profiles', _steamid);
+}; 
 
-function newSteamCache(steamid) {
-  console.log('[NEW STEAM CACHE]');
-  return Promise.join(buildSteamCache(steamid), getCacheFile(steamid, 'canvas.JSON'),
-    function(newSteamCache, canvasData) {
-      var userData = {};
-      userData = newSteamCache;
-      userData.canvas = canvasData;
+function compileCanvasData(data) {
+  var dataTemplate = getCanvasData();
+  delete data.steamid;
+  if (data.recentGameLogos) {
+    // Will be able to build an array
+    // Won't be limited to 2
+    data.recentGameLogo1 = {'active' : true};
+    data.recentGameLogo2 = {'active' : true};
+  }
+  delete data.recentGameLogos;
 
-      return Promise.join(saveCache(steamid, 'steam.JSON', newSteamCache), draw(userData), 
-        function() {console.log('[/NEW STEAM CACHE]');}
-      );
-    }
-  );
+  for (var element in data) {
+    dataTemplate.elements[element].active = true;
+  }
+
+  return dataTemplate;
 }
 
-function updateProfile(steamid) {
+function updateSteamProfile(steamData) {
   console.log('[UPDATE PROFILE]');
 
-  return Promise.join(getCache(steamid), buildSteamCache(steamid),
-    function(localCache, refreshedSteamCache) {
-      if (_.isEqual(localCache.steam, refreshedSteamCache.steam)) {
-        console.log('No changes to update. Skipping render.');
-        return saveCache(steamid, 'steam.JSON', refreshedSteamCache)
+  return getCacheFile('canvas.JSON')
 
-        .then(function(){console.log('[/UPDATE PROFILE]');});
+  .then(function(canvasData) {
+    var compiledProfileData = steamData;
+    compiledProfileData.canvas = canvasData;
 
-      } else {
-        console.log('Found changes. Re-rendering profile');
-
-        var newCache = {};
-        newCache = refreshedSteamCache;
-        newCache.canvas = localCache.canvas;
-
-        return Promise.join(saveCache(steamid, 'steam.JSON', refreshedSteamCache), draw(newCache)
-          , function() {
-
-            console.log('[/UPDATE PROFILE]');
-            // return saveCache(steamid, 'old.JSON', localCache.steam)
-
-            // .then(saveCache(steamid, 'new.JSON', refreshedSteamCache.steam));
-          }
-        );
-      }
-    }
-  );
+    return Promise.join(
+      saveCache('steam.JSON', steamData),
+      draw(compiledProfileData)
+    );
+  });
 }
 
-function isExpired(lastUpdateDate) {
+function isExpired(lastUpdateTimestamp) {
   var expireThreshhold = 10;
-  var lastUpdated = new Date(lastUpdateDate);
+  var lastUpdated = new Date(lastUpdateTimestamp);
   var currentDate = new Date();
   var secondsSinceProfileUpdate = Math.ceil((currentDate - lastUpdated) / 1000);
 
@@ -152,7 +127,7 @@ function isExpired(lastUpdateDate) {
   }
 }
 
-function buildSteamCache(steamid) {
+function buildSteamCache() {
   console.log('[BUILD STEAM DATA]');
 
   var steamAPIRequest = steam.buildRequest(
@@ -205,11 +180,11 @@ function buildSteamCache(steamid) {
   });
 }
 
-function saveCache(steamid, cacheLabel, data) {
+function saveCache(cacheLabel, data) {
   console.time('|>Cache user data');
   var dataString = JSON.stringify(data);
 
-  return getUserDirectory(steamid)
+  return getUserDirectory()
 
   .then(function(userDir) {
     var saveLocation = path.join(userDir, cacheLabel);
@@ -223,7 +198,7 @@ function saveCache(steamid, cacheLabel, data) {
   });  
 }
 
-function getCacheFile(steamid, fileName) {
+function getCacheFile(fileName) {
   console.log('GET:', steamid, fileName);
 
   var pathToCache = path.join('assets', 'profiles', steamid, fileName);
@@ -235,21 +210,23 @@ function getCacheFile(steamid, fileName) {
   .then(JSON.parse);
 }
 
-function getCache(steamid) {
-  return Promise.join(getCacheFile(steamid, 'canvas.JSON')
-    , getCacheFile(steamid, 'steam.JSON')
+function getCache() {
+  console.log('[GET CACHE]');
+
+  return Promise.join(getCacheFile('canvas.JSON')
+    , getCacheFile('steam.JSON')
     , function(canvasData, steamData) {
       var cache = {};
 
-      cache.canvas = canvasData;
       cache = steamData;
+      cache.canvas = canvasData;
 
       return Promise.resolve(cache);
     }
   );
 }
 
-function getUserDirectory(steamid) {
+function getUserDirectory() {
   var userDir = path.join('assets', 'profiles', steamid);
 
   return new Promise(function (resolve) {
